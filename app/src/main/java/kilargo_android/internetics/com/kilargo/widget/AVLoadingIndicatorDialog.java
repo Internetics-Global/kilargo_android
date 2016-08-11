27575ee16117d684d9bafeb2b47807b0462694fb
Internetics.net.au <Internetics2016@Bournes-MacBook-Pro.local>
2016-08-11 10:49:18 +1000

    fix a bug in Android 4.4 version (it's ok > 5.0)

--- app/src/main/java/kilargo_android/internetics/com/kilargo/widget/AVLoadingIndicatorDialog.java
+++ app/src/main/java/kilargo_android/internetics/com/kilargo/widget/AVLoadingIndicatorDialog.java
@@ -2,7 +2,9 @@ package kilargo_android.internetics.com.kilargo.widget;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
@@ -20,6 +22,11 @@ public class AVLoadingIndicatorDialog extends AlertDialog {
         super(context);
         View view = LayoutInflater.from(getContext()).inflate(R.layout.progress_indicator_avld, null);
         mMessageView = (TextView) view.findViewById(R.id.message);
+
+        //transparent dialog background does not support on <5.0
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            view.setBackgroundColor(Color.GRAY);
+        }
 
         setView(view,0,0,0,0);//remove border  http://stackoverflow.com/questions/10433764/alertdialog-how-to-remove-black-borders-above-and-below-view
         getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));  //http://stackoverflow.com/questions/8117849/remove-black-background-on-custom-dialog
